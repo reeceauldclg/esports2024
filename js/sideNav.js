84073de35438ad44425be5425347eb086ce476d5
reece <reeceauldclg@gmail.com>
2024-08-30 14:03:01 +0100

    Add files via upload

---
+++ js/sideNav.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("pageSideNav").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("pageSideNav").style.width = "0";
+}
---
+++ js/transition.js
@@ -0,0 +1,11 @@
+let headerBackgrounds = document.querySelectorAll(".background");
+let imageIndex = 0;
+function changeBackground(){
+  headerBackgrounds[imageIndex].classList.remove("showing");
+  imageIndex++;
+  if (imageIndex >= headerBackgrounds.length) {
+    imageIndex = 0;
+  }
+  headerBackgrounds[imageIndex].classList.add("showing");
+}
+setInterval(changeBackground, 10000);
